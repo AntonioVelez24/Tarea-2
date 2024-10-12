4d30993aadb42d2b64a1b1febbc528ca0df5f8b7
AntonioVelez24 <fabricio.velez@tecsup.edu.pe>
2024-10-12 00:47:58 -0500

    Menu

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public Button btnPlay;
+    public GameObject AudioPanel;
+    private bool activePanel = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,18 @@ public class MenuController : MonoBehaviour
     void Play()
     {
         SceneManager.LoadScene("Game");
+    }
+    public void ActivePanel()
+    {
+        if (activePanel == false)
+        {
+            activePanel = true;
+            AudioPanel.SetActive(true);
+        }
+        else
+        {
+            activePanel = false;
+            AudioPanel.SetActive(false);
+        }
     }
 }
